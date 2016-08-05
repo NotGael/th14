ed07e03e8a3904e05cac87f3839e8580fcacf177
Gaël Fontenelle <fontenelle.gael@gmail.com>
2016-08-05 15:33:36 +0200

    ortho
    porto

--- resources/views/main/photography.blade.php
+++ resources/views/main/photography.blade.php
@@ -20,7 +20,7 @@
             @else
                 <h3 class="text-center">Aucune image n'a été trouvée</h3>
                 <p class="text-center">Vous devez êtres inscrit sur le site et être un utilisateur validé pour pouvoir visionner des photos</p>
-                <p class="text-center">Veuillez vous inscrires si vous êtes scouts ou parents</p>
+                <p class="text-center">Veuillez vous inscrire si vous êtes scout ou parent</p>
                 <p class="text-center">Attendez que votre compte soit validé par un admin</p>
             @endif
         </div>
